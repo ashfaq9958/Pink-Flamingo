97d779d3de54f34fd59a2aa2337d9da3aea4e0b5
ashfaq9958 <137767028+ashfaq9958@users.noreply.github.com>
2024-10-05 03:15:25 +0530

    lets connect

--- src/components/Dashboard/HeroSection/HeroSection.jsx
+++ src/components/Dashboard/HeroSection/HeroSection.jsx
@@ -7,25 +7,48 @@ import {
   KeyboardArrowRightRounded,
 } from "@mui/icons-material";
 
-const HeroSection = () => (
-  <Carousel
-    indicators={false}
-    animation="fade"
-    interval={2500}
-    navButtonsAlwaysInvisible={true}
-  >    <div className="relative w-[90%] mx-auto h-[30vh] flex justify-center items-center gap-10 bg-[#FFEEF6]">
-      <img src={Hero_1} alt="hero_1" className="w-44 py-10 object-contain" />
-      <img src={Hero_2} alt="hero_2" className="w-1/3 object-contain" />
+const HeroSection = () => {
+  const hero = [
+    { img1: Hero_1, img2: Hero_2 },
+    { img1: Hero_1, img2: Hero_2 },
+  ];
 
-      <div className="absolute left-10 top-1/2 -translate-y-1/2 bg-white rounded-full w-8 h-8 flex justify-center items-center cursor-pointer">
-        <KeyboardArrowLeftRounded style={{ color: "#fa5c98" }} />
-      </div>
+  return (
+    <>
+      <Carousel
+        animation="fade"
+        interval={2500}
+        navButtonsAlwaysInvisible={true}
+        // autoPlay
+      >
+        {hero.map((item, index) => (
+          <div
+            key={index}
+            className="relative w-[90%] mx-auto h-[30vh] flex justify-center items-center gap-10 bg-[#FFEEF6]"
+          >
+            <img
+              src={item.img1}
+              alt="hero_1"
+              className="w-44 py-10 object-contain"
+            />
+            <img
+              src={item.img2}
+              alt="hero_2"
+              className="w-1/3 object-contain"
+            />
 
-      <div className="absolute right-10 top-1/2 -translate-y-1/2 bg-white rounded-full w-8 h-8 flex justify-center items-center cursor-pointer">
-        <KeyboardArrowRightRounded style={{ color: "#fa5c98" }} />
-      </div>
-    </div>
-  </Carousel>
-);
+            <div className="absolute left-10 top-1/2 -translate-y-1/2 bg-white rounded-full w-8 h-8 flex justify-center items-center cursor-pointer">
+              <KeyboardArrowLeftRounded style={{ color: "#fa5c98" }} />
+            </div>
+
+            <div className="absolute right-10 top-1/2 -translate-y-1/2 bg-white rounded-full w-8 h-8 flex justify-center items-center cursor-pointer">
+              <KeyboardArrowRightRounded style={{ color: "#fa5c98" }} />
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </>
+  );
+};
 
 export default HeroSection;
